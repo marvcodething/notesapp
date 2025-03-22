6bf2ab996cfdd533bf99bf084a5689915781fc00
marvcodething <mromero3@oxy.edu>
2025-03-21 20:18:48 -0700

    first few

--- src/app/page.js
+++ src/app/page.js
@@ -4,23 +4,36 @@ import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const tabContent = [
-  { title: "NOTE TO SELF", text: "A digital notepad in the palm of our hands: a space for our grocery lists, passwords, diary entries, and a host of other purposes, meant only for our eyes. The Apple Notes app has become ubiquitous, and yet the ways individuals utilize this application varies widely. The Spring ’25 cohort of MAC 242: Projects in Documentary Media invites you to explore the ways in which we utilize the Notes app, and how it takes shape in our daily lives. ", video: "", thumbnail: "", date: "February 1, 2025 at 10:00AM" },
-  { title: "Placeholder title for Tab 2", text: "Placeholder text for Tab 2", video: "video2.mp4", thumbnail: "thumbnail2.jpg", date: "February 15, 2025 at 2:30PM" },
-  { title: "Placeholder title for Tab 3", text: "Placeholder text for Tab 3", video: "video3.mp4", thumbnail: "thumbnail3.jpg", date: "March 3, 2025 at 1:00PM" },
-  { title: "Placeholder title for Tab 4", text: "Placeholder text for Tab 4", video: "video4.mp4", thumbnail: "thumbnail4.jpg", date: "March 10, 2025 at 4:45PM" },
-  { title: "Placeholder title for Tab 5", text: "Placeholder text for Tab 5", video: "video5.mp4", thumbnail: "thumbnail5.jpg", date: "February 20, 2025 at 9:15AM" },
-  { title: "Placeholder title for Tab 6", text: "Placeholder text for Tab 6", video: "video6.mp4", thumbnail: "thumbnail6.jpg", date: "March 5, 2025 at 11:30PM" },
-  { title: "Placeholder title for Tab 7", text: "Placeholder text for Tab 7", video: "video7.mp4", thumbnail: "thumbnail7.jpg", date: "February 28, 2025 at 3:00PM" },
-  { title: "Placeholder title for Tab 8", text: "Placeholder text for Tab 8", video: "video8.mp4", thumbnail: "thumbnail8.jpg", date: "March 12, 2025 at 12:00PM" },
-  { title: "Placeholder title for Tab 9", text: "Placeholder text for Tab 9", video: "video9.mp4", thumbnail: "thumbnail9.jpg", date: "February 25, 2025 at 5:30PM" },
-  { title: "Placeholder title for Tab 10", text: "Placeholder text for Tab 10", video: "video10.mp4", thumbnail: "thumbnail10.jpg", date: "March 15, 2025 at 8:00AM" },
-  { title: "Placeholder title for Tab 11", text: "Placeholder text for Tab 11", video: "video11.mp4", thumbnail: "thumbnail11.jpg", date: "February 5, 2025 at 6:00PM" },
-  { title: "Placeholder title for Tab 12", text: "Placeholder text for Tab 12", video: "video12.mp4", thumbnail: "thumbnail12.jpg", date: "March 20, 2025 at 10:30AM" },
-  { title: "Placeholder title for Tab 13", text: "Placeholder text for Tab 13", video: "video13.mp4", thumbnail: "thumbnail13.jpg", date: "February 10, 2025 at 7:45PM" },
-  { title: "Placeholder title for Tab 14", text: "Placeholder text for Tab 14", video: "video14.mp4", thumbnail: "thumbnail14.jpg", date: "March 25, 2025 at 1:15PM" },
-  { title: "Placeholder title for Tab 15", text: "Placeholder text for Tab 15", video: "video15.mp4", thumbnail: "thumbnail15.jpg", date: "February 18, 2025 at 4:00PM" },
-  { title: "Placeholder title for Tab 16", text: "Placeholder text for Tab 16", video: "video16.mp4", thumbnail: "thumbnail16.jpg", date: "March 30, 2025 at 9:00AM" },
+  { title: "NOTE TO SELF", text: "A digital notepad in the palm of our hands: a space for our grocery lists, passwords, diary entries, and a host of other purposes, meant only for our eyes. The Apple Notes app has become ubiquitous, and yet the ways individuals utilize this application varies widely. The Spring '25 cohort of MAC 242: Projects in Documentary Media invites you to explore the ways in which we utilize the Notes app, and how it takes shape in our daily lives. ", video: "", thumbnail: "/assets/thumbnails/mainThumbnail.jpg", date: "February 1, 2025 at 10:00AM" },
+  { title: "The Killer - Bea Irwin", text: "The killer!!!", video: "https://youtu.be/Y02cmiDO4Jg", thumbnail: "/assets/thumbnails/beathumbnail.png", date: "February 15, 2025 at 2:30PM" },
+  { title: "Writers Block - Eliana Joftus", text: "Idk maybe something like", video: "https://youtu.be/w7RkxP9lYoM", thumbnail: "/assets/thumbnails/ElianaJoftus-Thumbnail.jpg", date: "March 3, 2025 at 1:00PM" },
+  { title: "Handwritten Note - Sara Masaki", text: "           ", video: "https://youtu.be/w3TzJzH7g3o", thumbnail: "/assets/thumbnails/masaki_thumbnail.png", date: "March 10, 2025 at 4:45PM" },
+  { title: "My Notes App - Cielo", text: "January 1, 2025:", video: "https://youtu.be/fpV3l_d3PDo", thumbnail: "https://youtu.be/w3TzJzH7g3o", date: "January 1, 2025 at 9:15AM" },
+  { title: "Birthday Planning - Luella Ottmann", text: "Night 1:\n   - CELEBRATE AT MIDNIGHT - wherever we are\n   - Surprise party at my quad\n      - Spiderman decoration...\n     - Cake for sure\n      - Hats!!!", video: "https://youtu.be/JAqi2tfQbys", thumbnail: "/assets/thumbnails/birthday.png", date: "March 5, 2025 at 11:30PM" },
+  { title: "In The Script - Marvin Romero", text: "(just bought a new record) yeah this is my beater copy\n\nI want you to have s** with my wife\n\nGrady - this guy just told me you can put axe body spray on your butt and she can lick it and it tastes like alcohol - make sure you…", video: "https://youtu.be/AgeRE0JmzW8", thumbnail: "/assets/thumbnails/ROMERO_THUMBNAIL.jpeg", date: "February 28, 2025 at 3:00PM" },
+  { title: "Handwrapping - Naomi Bahn-Logan", text: "Loop over thumb - wrist x3 - thumb x2 - wrist x1 - knuckles x3 - wrist x1 - pointer finger - middle - ring - pinky (cross pattern) - knuckles x1 - leftover goes to wrist", video: "https://youtu.be/P1WQIFMI1eI", thumbnail: "thumbnail8.jpg", date: "March 12, 2025 at 12:00PM" },
+  { title: "Mourning While Living - Ruby King", text: "Dream Recording Oct 14, 2024\n\ni feel magga and the women she is now. for two nights in a row i visit her.\nfor two nights in a row a sob in my dream, i didn't even know that was possible.\nthis time mom and charles were there.\ni sobbed and said this is the year of regret", video: "https://youtu.be/nF0tfjGqZhw", thumbnail: "thumbnail9.jpg", date: "February 25, 2025 at 5:30PM" },
+  { title: "Dj Songs - Ava Zagoria", text: "- Money trees X panda\n- Nokia x modern jam\n- Panda x still don't know my name", video: "https://youtu.be/4qf7FVyU9DM", thumbnail: "thumbnail10.jpg", date: "March 15, 2025 at 8:00AM" },
+  { title: "Goals For Spring Break - Brown Schneider", text: "1. Take Bula for a walk\n2. Finally get a drivers license", video: "https://youtu.be/pIkkb5tqPpY", thumbnail: "thumbnail11.jpg", date: "February 5, 2025 at 6:00PM" },
+  { title: "Outfit Ideas - Meg Tomonari", text: "- Black t-shirt\n- Jeans\n- White long shirt\n- Green sweater\n- Black dress", video: "video12.mp4", thumbnail: "thumbnail12.jpg", date: "March 20, 2025 at 10:30AM" },
+  { title: "Remember Before You Play - Ella Sran", text: "The ball didn’t sound as good or kick as well because it was flat, so grab the soccer pump before playing with Emma next time. That should make it sound better.", video: "video13.mp4", thumbnail: "thumbnail13.jpg", date: "February 10, 2025 at 7:45PM" },
+  { title: "My (written) Notes App - Sloan Wittliff", text: "My notes app contains the most mundane things.\nBut buried between them are some of the most important things I've written for myself.", video: "video14.mp4", thumbnail: "thumbnail14.jpg", date: "March 25, 2025 at 1:15PM" },
+  { title: "Dreams - Jack Sabee-Paulson", text: "March 19 2025\n\nDream:", video: "video15.mp4", thumbnail: "thumbnail15.jpg", date: "March 19, 2025 at 6:00AM" },
+  { title: "Admin Day - Luca Sonne", text: "Laundry\nMeasure for measure act ii\n Clean room/bathroom\nTarget", video: "video16.mp4", thumbnail: "thumbnail16.jpg", date: "March 30, 2025 at 9:00AM" }
 ];
+
+// First, create a helper function to check if the video is a YouTube URL
+const isYouTubeUrl = (url) => url.includes('youtu.be') || url.includes('youtube.com');
+
+// Function to convert YouTube URL to embed URL
+const getYouTubeEmbedUrl = (url) => {
+  if (url.includes('youtu.be')) {
+    const videoId = url.split('youtu.be/')[1];
+    return `https://www.youtube.com/embed/${videoId}`;
+  }
+  return url;
+};
+
 export default function Home() {
   const [selectedTab, setSelectedTab] = useState(0);
   const [isMobile, setIsMobile] = useState(false);
@@ -79,7 +92,7 @@ export default function Home() {
 
       {/* Right Section: Content */}
       {!isMobile && (
-        <div className="w-3/4 bg-neutral-900 text-white">
+        <div className="w-3/4 bg-neutral-900 text-white overflow-y-auto h-screen">
           <div className="p-4">
             <h2 className="mb-2 text-sm text-center text-gray-500" style={{ fontFamily: "'SF Pro Display', sans-serif" }}>
               {tabContent[selectedTab].date} - View Only
@@ -94,11 +107,25 @@ export default function Home() {
                   {tabContent[selectedTab].text}
                 </div>
                 
-                {tabContent[selectedTab].video && (
-                  <video controls className="w-full h-auto px-4" poster={tabContent[selectedTab].thumbnail}>
-                    <source src={tabContent[selectedTab].video} type="video/mp4" />
-                    Your browser does not support the video tag.
-                  </video>
+                {selectedTab === 0 ? (
+                  <img src={tabContent[selectedTab].thumbnail} alt="Note to Self" className="w-full h-auto px-4" />
+                ) : (
+                  tabContent[selectedTab].video && (
+                    isYouTubeUrl(tabContent[selectedTab].video) ? (
+                      <iframe
+                        className="w-full aspect-video px-4"
+                        src={getYouTubeEmbedUrl(tabContent[selectedTab].video)}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                      ></iframe>
+                    ) : (
+                      <video controls className="w-full h-auto px-4" poster={tabContent[selectedTab].thumbnail}>
+                        <source src={tabContent[selectedTab].video} type="video/mp4" />
+                        Your browser does not support the video tag.
+                      </video>
+                    )
+                  )
                 )}
               </>
             )}
@@ -115,7 +142,7 @@ export default function Home() {
             animate={{ x: 0 }}
             exit={{ x: "100%" }}
             transition={{ type: "tween", duration: 0.3 }}
-            className="w-full bg-neutral-900 text-white p-4 fixed inset-0 z-50"
+            className="w-full bg-neutral-900 text-white p-4 fixed inset-0 z-50 overflow-y-auto"
           >
             <div onClick={handleBack} className="flex items-center mb-4 cursor-pointer">
               <span className="text-2xl mr-2">←</span> {/* Left-pointing arrow */}
@@ -126,10 +153,26 @@ export default function Home() {
             </h2>
             <h3 className="px-4 pt-4 text-lg font-extrabold">{tabContent[selectedTab].title}</h3>
             <div className="px-4 pb-10 rounded-lg text-white my-4" style={{ fontFamily: "'SF Pro Display', sans-serif", whiteSpace: "pre-line" }}>{tabContent[selectedTab].text}</div>
-            <video controls className="mt-5 w-full h-auto" poster={tabContent[selectedTab].thumbnail}>
-              <source src={tabContent[selectedTab].video} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
+            {selectedTab === 0 ? (
+              <img src={tabContent[selectedTab].thumbnail} alt="Note to Self" className="mt-5 w-full h-auto" />
+            ) : (
+              tabContent[selectedTab].video && (
+                isYouTubeUrl(tabContent[selectedTab].video) ? (
+                  <iframe
+                    className="w-full aspect-video px-4"
+                    src={getYouTubeEmbedUrl(tabContent[selectedTab].video)}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                ) : (
+                  <video controls className="mt-5 w-full h-auto" poster={tabContent[selectedTab].thumbnail}>
+                    <source src={tabContent[selectedTab].video} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                )
+              )
+            )}
           </motion.div>
         )}
       </AnimatePresence>
